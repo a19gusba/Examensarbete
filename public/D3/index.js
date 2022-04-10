7fb512b8e8614ef934fa0ff68dcc22a20bf99902
Gustav Bäckman <a19gusba@student.his.se>
2022-04-10 21:01:02 +0200

    use measure script and datahandler script in d3.js

--- public/D3/index.js
+++ public/D3/index.js
@@ -1,54 +1,101 @@
-// set the dimensions and margins of the graph
-var margin = { top: 30, right: 30, bottom: 30, left: 30 },
-    width = 450 - margin.left - margin.right,
-    height = 450 - margin.top - margin.bottom;
+/* generateChessBoard() */
+generateButtons()
 
-// append the svg object to the body of the page
-var svg = d3.select("#heatmap-container")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
+let data
+let formatedData
+let maxFrequency
+let squareFrequency
+
+
 
 // Labels of row and columns
-var myGroups = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
-var myVars = ["v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8", "v9", "v10"]
+var myGroups = ["a", "b", "c", "d", "e", "f", "g", "h"]
+var myVars = ["1", "2", "3", "4", "5", "6", "7", "8"]
+var svg;
+var x;
+var y;
 
-// Build X scales and axis:
-var x = d3.scaleBand()
-    .range([0, width])
-    .domain(myGroups)
-    .padding(0.01);
-svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x))
-
-// Build X scales and axis:
-var y = d3.scaleBand()
-    .range([height, 0])
-    .domain(myVars)
-    .padding(0.01);
-svg.append("g")
-    .call(d3.axisLeft(y));
 
 // Build color scale
-var myColor = d3.scaleLinear()
-    .range(["white", "#69b3a2"])
-    .domain([1, 100])
+var myColor
+
 
 //Read the data
-d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/heatmap_data.csv", function (data) {
+async function loadData() {
+    data = await getAllData()
+}
 
-    svg.selectAll()
-        .data(data, function (d) { return d.group + ':' + d.variable; })
-        .enter()
-        .append("rect")
-        .attr("x", function (d) { return x(d.group) })
-        .attr("y", function (d) { return y(d.variable) })
-        .attr("width", x.bandwidth())
-        .attr("height", y.bandwidth())
-        .style("fill", function (d) { return myColor(d.value) })
+async function formatData(piece) {
+    let movesData = await formatDataMoves(data)
+    formatedData = await formatDataSquares(movesData, piece)
 
-})
+    let [max, all] = await getMostFrequentSquare(formatedData)
+    maxFrequency = max
+    squareFrequency = all
+
+    myColor = d3.scaleLinear()
+        .range(["rgba(255, 255, 255, 0)", "#00C2D1"]) /* 00C2D1 DB504A*/
+        .domain([1, maxFrequency])
+}
+
+function setupSvg() {
+    document.getElementById("test").innerHTML = ""
+
+    // set the dimensions and margins of the graph
+    var margin = { top: 30, right: 30, bottom: 30, left: 30 },
+        width = 450 - margin.left - margin.right,
+        height = 450 - margin.top - margin.bottom;
+
+    // append the svg object to the body of the page
+    svg = d3.select("#test")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+    // Build X scales and axis:
+    x = d3.scaleBand()
+        .range([0, width])
+        .domain(myGroups)
+
+    svg.append("g")
+        .style("font-size", 15)
+        .style("stroke", "white")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x))
+
+    // Build Y scales and axis:
+    y = d3.scaleBand()
+        .range([height, 0])
+        .domain(myVars)
+
+    svg.append("g")
+        .call(d3.axisLeft(y))
+        .style("font-size", 15)
+        .style("stroke", "white");
+}
+
+function render() {
+    let data = squareFrequency
+    setupSvg()
+
+
+    for (let square in data) {
+        svg.selectAll()
+            .data(square)
+            .enter()
+            .append("rect")
+            .attr("x", () => { return x(square[0]) })
+            .attr("y", () => { return y(square[1]) })
+            .attr("rx", 4)
+            .attr("ry", 4)
+            .attr("width", x.bandwidth())
+            .attr("height", y.bandwidth())
+            .style("fill", function (d) { return myColor(squareFrequency[square]) })
+            .style("stroke-width", 4)
+            .style("stroke", "none")
+    }
+
+}
