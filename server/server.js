40a3697f55044c586cae2701de87a1b9306c5661
Gustav Bäckman <a19gusba@student.his.se>
2022-04-10 16:20:29 +0200

    added nodemon

--- server/server.js
+++ server/server.js
@@ -3,8 +3,10 @@ const mongoose = require("mongoose")
 const cors = require("cors")
 
 const app = express()
+const dataRouter = require("./routes/chess")
 
 app.use(cors())
+app.use("/data/", dataRouter)
 
 app.listen(5000, console.log("server started"))
 
